d0556211dcebe5b4a2f9eee167cc55922060ca4d
jambik <jambik@gmail.com>
2016-05-27 22:42:28 +0300

    - настройка движка

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -18,9 +18,9 @@ class IndexController extends FrontendController
     {
         $page = Page::find(1);
 
-        $products1 = Product::whereCategoryId('1')->limit(3)->get();
-        $products2 = Product::whereCategoryId('2')->limit(3)->get();
-        $products3 = Product::whereCategoryId('3')->limit(3)->get();
+        $products1 = Product::whereCategoryId('80')->limit(3)->get();
+        $products2 = Product::whereCategoryId('103')->limit(3)->get();
+        $products3 = Product::whereCategoryId('56')->limit(3)->get();
 
         return view('index', compact('page', 'products1', 'products2', 'products3'));
     }
